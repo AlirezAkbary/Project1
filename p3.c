22ac1d7ad4d1a361d4c38112b54096c1869b0096
ali-bana <alibanaei@live.com>
2016-12-12 17:28:15 +0330

    changed by me

--- p3.c
+++ p3.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+int IsPrime(int n)
+{
+    int i, sqrt;
+    sqrt = sqrt(n);
+    for(i = 2; i <= sqrt; i++)
+    {
+        if(n % i == 0)
+            return 0;
+
+    }
+    return 1;
+}
+
 int main()
 {
-    printf("hooooraaaay");
+    int n = 0;
+    scanf("%d", &n);
+    printf("%d", IsPrime(n));
 }
