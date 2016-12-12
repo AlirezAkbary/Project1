1a4539c99e2032147a514ca24fc4c492c8607a5e
AlirezAkbary <alireza_akbari34@yahoo.com>
2016-12-12 17:30:07 +0330

    Ifindargmax!

--- p3.c
+++ p3.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-
+int findargmax(int a[], length)
+{
+    int i = 0, max = 0;
+    for ( i = 0; i < length; i++)
+        if (a[i] > a[max])
+            max = i;
+    return max;
+}
 int main()
 {
-    printf("hooooraaaay");
+    int arr[5] = {1, 2, 3, 4, 5};
+    int max = findargmax(arr, 5);
+    printf("%d", max);
 }
