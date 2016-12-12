8f66ea02c9efb00a0bbe0683b7ea2a546c210f52
ali-bana <alibanaei@live.com>
2016-12-12 16:53:00 +0330

    added is prime# Please enter the commit message for your changes. Lines starting

--- p1.c
+++ p1.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+#include<math.h>
+int IsPrime(int n)
+{
+    int i, sqrt;
+    sqrt = sqrt(n);
+    for(i = 2; i <= sqrt; i++)
+    {
+        if(n % i == 0)
+            return 0;
+
+    }
+    return 1;
+}
 
 int main()
 {
